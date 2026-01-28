c1f8ec67b7971319cac7c8ce339f3f99c9d9aa78
Duncan McClean <duncan@duncanmcclean.com>
2026-01-28 17:02:14 +0000

    Update for Statamic 6 (#1)
    * Update for Statamic 6
    
    * require stable

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import statamic from '@statamic/cms/vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        statamic(),
+        laravel({
+            input: ['resources/js/cp.js'],
+            refresh: true,
+            publicDirectory: 'resources/dist',
+            hotFile: 'resources/dist/hot',
+        }),
+    ],
+});
--- webpack.mix.js
+++
@@ -1,3 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('src/resources/js/addon.js', 'dist/js').vue;
